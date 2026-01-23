72f2253a0a6e3f3e8e66fafb2b165d201669e723
Gustavo Rivera <garp2100@gmail.com>
2026-01-23 01:55:13 -0500

    fixing swagger configuration logic to display properly

--- Program.cs
+++ Program.cs
@@ -64,7 +64,6 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
