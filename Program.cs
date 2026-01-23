7328b156cacd0ab9475ca6b6caefe23a4ecf3fa1
Gustavo Rivera <garp2100@gmail.com>
2026-01-23 01:47:37 -0500

    .gitignore and Dockerfile

--- Program.cs
+++ Program.cs
@@ -65,15 +65,12 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "TestableWebApp API v1");
+    c.RoutePrefix = "swagger";
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
